15e8417e4ac62fd9fba65087494056e0a0470b6f
lemiskutswa <kutswalemis16@gmail.com>
2023-01-20 16:54:23 +0300

    splitting code into functions cont'

--- basics-01-starting-project/assets/scripts/app.js
+++ basics-01-starting-project/assets/scripts/app.js
@@ -1,36 +1,51 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+//Gets input from input field. 
 function getUserInput() {
     return parseInt(userInput.value);
 }
 
-function jibberish() {}
+//A function to generate and write the output/log of the calculator
+function createAndWriteOutput(operator, resBeforeCalc, calcNumber) {
+    const calcDescription = `${resBeforeCalc} ${operator} ${calcNumber}`;
+    outputResult(currentResult, calcDescription);
+}
 
+
+//Basic math operation functions.
 function add() {
     const enteredNumber = getUserInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult = currentResult + enteredNumber;
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('+', initialResult, enteredNumber);
 }
 
 function subtract() {
     const enteredNumber = getUserInput();
-    const calcDescription = `${currentResult} - ${enteredNumber}`;
+    const initialResult = currentResult;
     currentResult = currentResult - enteredNumber;
-    outputResult(currentResult, calcDescription);
+    createAndWriteOutput('-', initialResult, enteredNumber);
 }
 
 function multiply() {
-  const enteredNumber = getUserInput();
-  const calcDescription = `${currentResult}* ${enteredNumber}`;
-  currentResult = currentResult*enteredNumber;
-  outputResult(currentResult, calcDescription);
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
 }
 
-function divide() {}
+function divide() {
+    const enteredNumber = getUserInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
 
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
 
 
 
