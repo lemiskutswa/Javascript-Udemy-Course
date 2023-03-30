f3ecbef3e9ff20248ebd556710ad51ed8d3469ff
lemiskutswa <kutswalemis16@gmail.com>
2023-03-30 12:35:40 +0300

    adding monster attack function

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,5 +1,18 @@
 const ATTACK_VALUE = 10;
+const MONSTER_ATTACK_VALUE = 14;
 
 let chosenMaxLife = 100;
+currentMonsterHealth = chosenMaxLife;
+currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+function attackHandler() {
+    const damage = dealMonsterDamage(ATTACK_VALUE);
+    currentMonsterHealth -= damage; 
+    if (currentMonsterHealth <= 0) {
+        alert('You Won!');
+    } 
+}
+
+attackBtn.addEventListener('click', attackHandler);
