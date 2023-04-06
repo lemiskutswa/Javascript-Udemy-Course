10749f31c06ad18fb1c71ffab34d7e5a86dc7a03
lemiskutswa <kutswalemis16@gmail.com>
2023-04-06 13:10:09 +0300

    heal function logic

--- control-03-monster-killer-starting-project/assets/scripts/app.js
+++ control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,12 +1,29 @@
 const ATTACK_VALUE = 10;
 const STRONG_ATTACK_VALUE = 17;
 const MONSTER_ATTACK_VALUE = 14;
+const HEAL_VALUE = 20;
 
 let chosenMaxLife = 100;
 currentMonsterHealth = chosenMaxLife;
 currentPlayerHealth = chosenMaxLife;
 
 adjustHealthBars(chosenMaxLife);
+
+//deals player damage and checks win condition after every attack/heal
+function endRound() {
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('You Won!');
+    } 
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('You Lost!');
+    }
+    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('You have a draw!');
+    }
+}
 
 function attackMonster(mode){
     let maxDamage;
@@ -19,19 +36,7 @@ function attackMonster(mode){
 
     const damage = dealMonsterDamage(maxDamage);
     currentMonsterHealth -= damage; 
-
-    const playerDamage = dealPlayerDamage(maxDamage);
-    currentPlayerHealth -= playerDamage;
-    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
-        alert('You Won!');
-    } 
-    else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
-        alert('You Lost!');
-    }
-    else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
-        alert('You have a draw!');
-    }
-
+    endRound();
 }
 
 function attackHandler() {
@@ -42,5 +47,23 @@ function strongAttackHandler() {
     attackMonster('STRONG_ATTACK');
 }
 
+function healPlayerHandler() {
+    let healValue;
+    if (currentPlayerHealth >= chosenMaxLife - HEAL_VALUE) {
+        alert("You can't heal more than your max initial health."); 
+        healValue = chosenMaxLife - currentPlayerHealth;
+    } else {
+        healValue = HEAL_VALUE;
+    }
+   
+    increasePlayerHealth(HEAL_VALUE);
+    currentPlayerHealth += HEAL_VALUE;
+    endRound();
+}
+
+//we want to let the monster attack us on every click, whether it is attack, strong attack, or heal. 
+//Therefore, deal player damage + checking win condition also needs to run. 
+
 attackBtn.addEventListener('click', attackHandler);
 strongAttackBtn.addEventListener('click', strongAttackHandler);
+healBtn.addEventListener('click', healPlayerHandler);
