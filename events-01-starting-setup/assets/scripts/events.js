cec81a7ed3a40f5f40985aacee7b96c90c369f9d
lemiskutswa <kutswalemis16@gmail.com>
2023-11-17 09:02:58 +0300

    Click events

--- events-01-starting-setup/assets/scripts/events.js
+++ events-01-starting-setup/assets/scripts/events.js
@@ -6,10 +6,20 @@ const onclickHandler = () => {
 }
 
 const anotherOnclickHandler = function () {
-    console.log('');
+    console.log('Was clicked');
 }
 
-button.onclick = onclickHandler;
+button.addEventListener('click', onclickHandler);
+
+setTimeout(()=> {
+    button.removeEventListener('click', onclickHandler); // if you add a new anonymous function instead of onclick handler, the function will never be removed. 
+    //This is because it would be a different function object from the onclickHandler. 
+}, 2000)
+button.addEventListener('click', anotherOnclickHandler);
+//dblclick - double click
+//context menu - right click
+// button.onclick = onclickHandler;//This button call comes after initialization because arrow functions can't be hoisted. 
+// button.onclick = anotherOnclickHandler;// This overrides the previous onclick. To avoid this use addEventListner - it allows you to have mutltiple event listeners. 
 
 
 
